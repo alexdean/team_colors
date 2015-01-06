fced71a9bff9f606e54589eb31f7611f7ff6fb95
Alex Dean <alex@crackpot.org>
2015-01-06 14:09:13 -0600

    add a set of interesting ad hoc queries you might want to run

---
+++ queries.sql
@@ -0,0 +1,23 @@
+# see distribution of distance values
+SELECT euclidian_dist, count(*)
+FROM diffs
+GROUP BY euclidian_dist
+ORDER BY euclidian_dist;
+
+# see similarites for each person
+SELECT raw1.name, raw2.name, diffs.euclidian_dist, diffs.orange_diff, diffs.gold_diff, diffs.green_diff, diffs.blue_diff
+FROM diffs
+  INNER JOIN raw raw1 ON (diffs.raw_id_1 = raw1.id)
+  INNER JOIN raw raw2 ON (diffs.raw_id_2 = raw2.id)
+ORDER BY
+  raw1.name, euclidian_dist;
+
+# who is most similar/different?
+SELECT raw1.name, raw2.name, diffs.euclidian_dist, diffs.orange_diff, diffs.gold_diff, diffs.green_diff, diffs.blue_diff
+FROM diffs
+  INNER JOIN raw raw1 ON (diffs.raw_id_1 = raw1.id)
+  INNER JOIN raw raw2 ON (diffs.raw_id_2 = raw2.id)
+WHERE
+  `unique` = 1
+ORDER BY
+  euclidian_dist, raw1.name, raw2.name
